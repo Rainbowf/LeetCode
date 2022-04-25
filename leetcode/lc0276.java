8004d051301bc7cd79bf6a60266b4993c666f082
Ryan1997 <527564197@qq.com>
2022-04-25 21:50:34 +0800

    第28次提交20220425

--- leetcode/lc0276.java
+++ leetcode/lc0276.java
@@ -8,7 +8,9 @@ class Solution0276 {
         int[][] dp = new int[n + 1][2];
         dp[0][1] = k;//第一个栅栏有k种涂法（包含0和1两种情况）
         for (int i = 1; i < n; i++) {
+            //与前一个栅栏同色
             dp[i][0] = dp[i - 1][1];
+            //与前一个栅栏不同色
             dp[i][1] = (dp[i - 1][0] + dp[i - 1][1]) * (k - 1);
         }
         return dp[n - 1][0] + dp[n - 1][1];
