6e85eb662678014f8105da56363d56c2a2828899
Ryan1997 <527564197@qq.com>
2022-04-26 11:30:37 +0800

    第29次提交20220426

--- leetcode/lc0152.java
+++ leetcode/lc0152.java
@@ -5,20 +5,18 @@ import java.util.*;
 //https://leetcode-cn.com/problems/maximum-product-subarray/solution/cheng-ji-zui-da-zi-shu-zu-by-leetcode-solution/401902
 class Solution0152 {
     public int maxProduct(int[] nums) {
-        int length = nums.length;
-        int[] maxF = new int[length];
-        int[] minF = new int[length];
-        System.arraycopy(nums, 0, maxF, 0, length);
-        System.arraycopy(nums, 0, minF, 0, length);
-        for (int i = 1; i < length; ++i) {
-            maxF[i] = Math.max(maxF[i - 1] * nums[i], Math.max(nums[i], minF[i - 1] * nums[i]));
-            minF[i] = Math.min(minF[i - 1] * nums[i], Math.min(nums[i], maxF[i - 1] * nums[i]));
+        int len = nums.length;
+        int[] maxP = new int[len];
+        int[] minP = new int[len];
+        int res;
+        maxP[0] = minP[0] = res = nums[0];
+        for(int i = 1; i < len; i++){
+            int curNum = nums[i];
+            maxP[i] = Math.max(   curNum, Math.max(   curNum * maxP[i-1], curNum * minP[i-1]  ));
+            minP[i] = Math.min(   curNum, Math.min(   curNum * maxP[i-1], curNum * minP[i-1]  ));
+            res = Math.max(  res,  maxP[i]  );
         }
-        int ans = maxF[0];
-        for (int i = 1; i < length; ++i) {
-            ans = Math.max(ans, maxF[i]);
-        }
-        return ans;
+        return res;
     }
     //滚动数组，优化
     public int maxProduct2(int[] nums) {
